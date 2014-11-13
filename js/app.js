a509e953e05b21e3b29e81955dcc77c49d3b2211
Stan Smith <stancsmith@gmail.com>
2014-11-13 15:18:41 -0500

    add remote js library refs, create index.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 var App = Ember.Application.create({ 
-	LOG_TRANSITIONS: true //this will log a message for every new page load
+	LOG_TRANSITIONS: true //this will log a message for every new page loads
 });
